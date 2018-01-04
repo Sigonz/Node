0aa8857540ca327e7a42e674b3fa7d4e6ccdebe6
sigonz <sigonz@gmail.com>
2018-01-04 10:55:09 -0500

    Update

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,24 +1,25 @@
 var mongoose = require('mongoose');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  },
- _creator:{
-     type: mongoose.Schema.Types.ObjectId,
-     required:true,
-    }
-});
+const TodoSchema = new mongoose.Schema({
+        text: {
+            type: String,
+            required: true,
+            minlength: 1,
+            trim: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        },
+        completedAt: {
+            type: Number,
+            default: null
+        },
+        _creator: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        }
+    });
+var Todo = mongoose.model('Todo',TodoSchema);
 
 module.exports = {Todo};
